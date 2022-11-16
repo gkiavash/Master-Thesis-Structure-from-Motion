dc7924baa489313a6850eea1ec67b477705668fe
gkiavash <gkiavash@gmail.com>
2022-11-16 16:01:48 +0100

    prepare for cluster 6

--- docker/pixsfm_job.sh
+++ docker/pixsfm_job.sh
@@ -6,7 +6,7 @@
 #SBATCH --mail-user gkiavash@gmail.com
 #SBATCH --mail-type ALL
 
-#SBATCH --time 04:00:00
+#SBATCH --time 12:00:00
 #SBATCH --ntasks 1
 #SBATCH --partition allgroups
 
@@ -17,4 +17,4 @@
 cd $WORKING_DIR
 #your working directory
 
-srun srun singularity exec --nv Master-Thesis-Structure-from-Motion/sif_files/pixsfm_test.sif /bin/bash Master-Thesis-Structure-from-Motion/scripts/colmap_pixsfm.sh Master-Thesis-Structure-from-Motion/colmap_project Master-Thesis-Structure-from-Motion/scripts/pixsfm_existing_db.py
+srun singularity exec --nv Master-Thesis-Structure-from-Motion/sif_files/pixsfm_test.sif /bin/bash Master-Thesis-Structure-from-Motion/scripts/colmap_pixsfm.sh Master-Thesis-Structure-from-Motion/colmap_project Master-Thesis-Structure-from-Motion/scripts/pixsfm_existing_db.py
