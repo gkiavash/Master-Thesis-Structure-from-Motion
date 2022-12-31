708cc33875e1b9614b7d80422f6048d26202c34f
gkiavash <gkiavash@gmail.com>
2022-12-31 18:17:32 +0100

    camera params in colmap 1

--- scripts/colmap_sparse.sh
+++ scripts/colmap_sparse.sh
@@ -7,7 +7,7 @@ colmap feature_extractor \
    --SiftExtraction.max_num_features 32768 \
    --ImageReader.single_camera 1 \
    --ImageReader.camera_model OPENCV_FISHEYE \
-   --ImageReader.camera_params -2.6434196998339271e-01,9.9571471417386093e-02,-2.4160314862664079e-04,-2.2267220647390027e-04,-1.9631169477800196e-02,0.,0.,0.
+   --ImageReader.camera_params 2.2880100739220425e+03,2.2845293722010042e+03,2.5716169994772490e+03,1.9205706165878657e+03,-2.6434196998339271e-01,9.9571471417386093e-02,-2.4160314862664079e-04,-2.2267220647390027e-04,-1.9631169477800196e-02,0.,0.,0.
 
 # Sequential Match
 colmap sequential_matcher \
