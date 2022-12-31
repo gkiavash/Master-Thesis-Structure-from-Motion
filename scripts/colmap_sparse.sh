1de3f6a948543f75f3846d97cd057876b4799e73
gkiavash <gkiavash@gmail.com>
2022-12-31 23:12:01 +0100

    camera params in colmap 4

--- scripts/colmap_sparse.sh
+++ scripts/colmap_sparse.sh
@@ -4,15 +4,15 @@ colmap feature_extractor \
    --database_path $DATASET_PATH/database.db \
    --image_path $DATASET_PATH/images \
    --SiftExtraction.max_image_size 10000 \
-   --SiftExtraction.max_num_features 32768 \
+   --SiftExtraction.max_num_features 50000 \
    --ImageReader.single_camera 1 \
-   --ImageReader.camera_model FULL_OPENCV \
-   --ImageReader.camera_params "2288.0100739220425,2284.529372201004,2571.616999477249,1920.5706165878657,-0.2643419699833927,0.09957147141738609,-0.0002416031486266408,-0.00022267220647390027,-0.019631169477800196,0,0,0"
+   --ImageReader.camera_model OPENCV \
+   --ImageReader.camera_params "2288.0100739220425,2284.529372201004,2571.616999477249,1920.5706165878657,-0.2643419699833927,0.09957147141738609,-0.0002416031486266408,-0.00022267220647390027"
 
 # Sequential Match
 colmap sequential_matcher \
    --database_path $DATASET_PATH/database.db \
-   --SiftMatching.max_num_matches 32768
+   --SiftMatching.max_num_matches 50000
 
 mkdir $DATASET_PATH/sparse
 
