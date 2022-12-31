1cad7b748fcf8eb7ceedb181c5ecb13c93ae0017
gkiavash <gkiavash@gmail.com>
2022-12-31 23:53:14 +0100

    camera params in colmap 5, change to previous calib

--- scripts/colmap_sparse.sh
+++ scripts/colmap_sparse.sh
@@ -7,7 +7,7 @@ colmap feature_extractor \
    --SiftExtraction.max_num_features 50000 \
    --ImageReader.single_camera 1 \
    --ImageReader.camera_model OPENCV \
-   --ImageReader.camera_params "2288.0100739220425,2284.529372201004,2571.616999477249,1920.5706165878657,-0.2643419699833927,0.09957147141738609,-0.0002416031486266408,-0.00022267220647390027"
+   --ImageReader.camera_params "2303.14339,2312.64462,2562.53024,1925.10736,-0.25332645,0.09693553,-0.00118667,-0.00139854"
 
 # Sequential Match
 colmap sequential_matcher \
@@ -19,4 +19,5 @@ mkdir $DATASET_PATH/sparse
 colmap mapper \
     --database_path $DATASET_PATH/database.db \
     --image_path $DATASET_PATH/images \
-    --output_path $DATASET_PATH/sparse
+    --output_path $DATASET_PATH/sparse \
+    --Mapper.ba_refine_principal_point 1
