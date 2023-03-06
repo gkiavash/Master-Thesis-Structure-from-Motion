ab912ba6fa08a15bf74dd1ea83391e1d3b07bb85
gkiavash <gkiavash@gmail.com>
2023-03-06 13:40:41 +0100

    try with undistorted images

--- scripts/colmap_sparse.sh
+++ scripts/colmap_sparse.sh
@@ -1,23 +1,32 @@
 DATASET_PATH=$1
+is_distorted=$2
 
-CALIB_PARAMS_OPENCV="2303.14339,2312.64462,1352,769,-0.25332645,0.09693553,-0.00118667,-0.00139854,-0.02359868,0,0,0"
-CALIB_PARAMS_YAML="2288.0100739220425,2284.529372201004,1352,769,-0.2643419699833927,0.09957147141738609,-0.0002416031486266408,-0.00022267220647390027,-0.019631169477800196,0,0,0"
 
 # The calib params below are obtained from the checkerboard dataset taken by camera with video settings
-CALIB_PARAMS_YAML_3="1267.305126032294,1161.14123676566,1370.030430369817,719.7204648074699,-0.22329932675609362,0.25083021808893313,-0.00243685822518905,0.0028180643499969675,-0.15365831433891083,0,0,0"
 CALIB_PARAMS_YAML_4="1333.0333496269382,1152.7953767273623,1362.2385792503271,705.0287442757885,-0.1697821786723204,0.08551140923416184,-0.0019370125115926041,-0.005935762788516092,-0.02775070400965532,0,0,0"
 
 #// Full OpenCV camera model.
 #// fx, fy, cx, cy, k1, k2, p1, p2, k3, k4, k5, k6
 
-colmap feature_extractor \
-   --database_path $DATASET_PATH/database.db \
-   --image_path $DATASET_PATH/images \
-   --SiftExtraction.max_image_size 10000 \
-   --SiftExtraction.max_num_features 50000 \
-   --ImageReader.single_camera 1 \
-   --ImageReader.camera_model FULL_OPENCV \
-   --ImageReader.camera_params $CALIB_PARAMS_YAML_4
+if [[ $is_distorted -eq 1 ]]
+then
+    colmap feature_extractor \
+       --database_path $DATASET_PATH/database.db \
+       --image_path $DATASET_PATH/images \
+       --SiftExtraction.max_image_size 10000 \
+       --SiftExtraction.max_num_features 50000 \
+       --ImageReader.single_camera 1 \
+       --ImageReader.camera_model FULL_OPENCV \
+       --ImageReader.camera_params $CALIB_PARAMS_YAML_4
+else
+    colmap feature_extractor \
+       --database_path $DATASET_PATH/database.db \
+       --image_path $DATASET_PATH/images \
+       --SiftExtraction.max_image_size 10000 \
+       --SiftExtraction.max_num_features 50000 \
+       --ImageReader.single_camera 1
+fi
+
 
 # Sequential Match
 colmap sequential_matcher \
--- scripts/utils/pose_prepare.py
+++ scripts/utils/pose_prepare.py
@@ -1,9 +1,10 @@
-PATH_MODEL = "/home/gkiavash/Downloads/sfm_projects/sfm_compare_4_pp_calib_3_prato/sfm_compare_pp_calib/sparse/0/export_as_txt/images.txt"
-PATH_OUTPUT = "/home/gkiavash/Downloads/sfm_projects/sfm_compare_4_pp_calib_3_prato/sfm_compare_pp_calib/sparse/0/export_as_txt/images_raw.txt"
+def prepare_images_txt(path_input, path_output):
+    path_input = "/home/gkiavash/Downloads/sfm_projects/sfm_compare_4_pp_calib" \
+                 "/sfm_compare_pp_calib_4/sparse/model_less_feat/0/images.txt"
+    path_output = "/home/gkiavash/Downloads/sfm_projects/sfm_compare_4_pp_calib" \
+                  "/sfm_compare_pp_calib_4/sparse/model_less_feat/0/images_raw.txt"
 
-
-def prepare_images_txt(path_model, path_output):
-    with open(path_model) as f:
+    with open(path_input) as f:
         lines = f.readlines()
 
     images_txt_new = []
@@ -18,4 +19,36 @@ def prepare_images_txt(path_model, path_output):
         outfile.write("\n".join(images_txt_new))
 
 
-prepare_images_txt(PATH_MODEL, PATH_OUTPUT)
+def pose_permute(path_input, path_output):
+    """
+    :param path_input: images.txt path
+    :param path_output: permuted
+    :return:
+    """
+    path_input = "/home/gkiavash/Downloads/sfm_projects/sfm_compare_4_pp_calib" \
+                 "/sfm_compare_pp_calib_4/sparse/model/images.txt"
+    path_output = "/home/gkiavash/Downloads/sfm_projects/sfm_compare_4_pp_calib" \
+                  "/sfm_compare_pp_calib_4/sparse/model/images_permuted.txt"
+
+    def add_noise(x, boundry=0.1):
+        import random
+        var = random.uniform(-boundry, boundry)
+        x += x * var
+        return x
+
+    with open(path_input) as f:
+        lines = f.readlines()
+        for index, line in enumerate(lines):
+            if line != "\n":
+                cols = line.split(" ")
+                print("old", cols)
+                for col in (
+                        # 1, 2, 3, 4,
+                        5, 6, 7
+                ):
+                    cols[col] = str(add_noise(float(cols[col]), 0.1))
+                lines[index] = " ".join(cols)
+                print("new", cols)
+
+        with open(path_output, "w") as fw:
+            fw.writelines(lines)
