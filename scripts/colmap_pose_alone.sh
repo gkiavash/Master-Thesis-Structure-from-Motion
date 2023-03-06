42ce989c729a71d67e64992d704603c658078ae7
gkiavash <gkiavash@gmail.com>
2023-03-06 14:14:24 +0100

    try with undistorted images 1

--- scripts/colmap_pose_alone.sh
+++ scripts/colmap_pose_alone.sh
@@ -11,20 +11,33 @@
 
 
 DATASET_PATH=$1
+is_distorted=$2
+max_num_features=$3
 
 CALIB_PARAMS_YAML="1333.0333496269382,1152.7953767273623,1362.2385792503271,705.0287442757885,-0.1697821786723204,0.08551140923416184,-0.0019370125115926041,-0.005935762788516092,-0.02775070400965532,0,0,0"
 
 #// Full OpenCV camera model.
 #// fx, fy, cx, cy, k1, k2, p1, p2, k3, k4, k5, k6
 
-colmap feature_extractor \
-   --database_path $DATASET_PATH/database.db \
-   --image_path $DATASET_PATH/images \
-   --SiftExtraction.max_image_size 10000 \
-   --SiftExtraction.max_num_features 50000 \
-   --ImageReader.single_camera 1 \
-   --ImageReader.camera_model FULL_OPENCV \
-   --ImageReader.camera_params $CALIB_PARAMS_YAML
+if [[ $is_distorted -eq 1 ]]
+then
+  colmap feature_extractor \
+     --database_path $DATASET_PATH/database.db \
+     --image_path $DATASET_PATH/images \
+     --SiftExtraction.max_image_size 10000 \
+     --SiftExtraction.max_num_features $max_num_features \
+     --ImageReader.single_camera 1 \
+     --ImageReader.camera_model FULL_OPENCV \
+     --ImageReader.camera_params $CALIB_PARAMS_YAML
+else
+  colmap feature_extractor \
+     --database_path $DATASET_PATH/database.db \
+     --image_path $DATASET_PATH/images \
+     --SiftExtraction.max_image_size 10000 \
+     --SiftExtraction.max_num_features $max_num_features \
+     --ImageReader.single_camera 1
+fi
+
 
 # Sequential Match
 colmap sequential_matcher \
