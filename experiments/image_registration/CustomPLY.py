700c680aeb101f9352ece0a26c11ab76a08c4e70
gkiavash <gkiavash@gmail.com>
2023-05-22 16:34:50 +0200

    ply preprocessing for registration

---
+++ experiments/image_registration/CustomPLY.py
@@ -0,0 +1,131 @@
+import copy
+from functools import cached_property
+import os
+from pathlib import Path
+
+import open3d as o3d
+import numpy as np
+
+
+class CustomPLY:
+    def __init__(self, ply_path):
+        self.ply_path = ply_path
+        self.ply = o3d.io.read_point_cloud(self.ply_path)
+
+    @cached_property
+    def radius(self):
+        distances = self.ply.compute_nearest_neighbor_distance()
+        avg_dist = np.mean(distances)
+        radius = 3 * avg_dist
+        print("radius:", radius)
+        return radius
+
+    def down_sampling(self, overwrite=True, preview=True):
+        ply_uniformed = self.ply.voxel_down_sample(voxel_size=self.radius)
+
+        if overwrite:
+            self.ply = ply_uniformed
+
+        if preview:
+            o3d.visualization.draw_geometries([ply_uniformed])
+
+        return ply_uniformed
+
+    def move_center(self):
+        self.ply.translate([0, 0, 0], relative=False)
+
+    def scale(self, scale):
+        self.ply.scale(scale, center=self.ply.get_center())
+
+    def show_range_coords(self):
+        ply_np = np.asarray(self.ply.points)
+        print()
+        for i in range(3):
+            print(
+                ply_np[:, i].min(),
+                ply_np[:, i].max(),
+                "range:",
+                ply_np[:, i].max() - ply_np[:, i].min()
+            )
+
+    def align_to_z(self, z_=-1, preview=True):
+        plane_model, inliers = self.ply.segment_plane(
+            distance_threshold=self.radius,
+            ransac_n=1000,
+            num_iterations=1000
+        )
+        [a, b, c, d] = plane_model
+        print(f"Plane equation: {a:.2f}x + {b:.2f}y + {c:.2f}z + {d:.2f} = 0")
+
+        plane_n = np.array([a, b, c])
+        plane_z = np.array([0, 0, z_])
+
+        vec1 = np.reshape(plane_n, (1, -1))
+        vec2 = np.reshape(plane_z, (1, -1))
+
+        from scipy.spatial.transform import Rotation as R
+        r = R.align_vectors(vec2, vec1)
+        rot = r[0].as_matrix()
+
+        ply_aligned = copy.deepcopy(self.ply)
+        ply_aligned.rotate(rot, center=ply_aligned.get_center())
+
+        if preview:
+            o3d.visualization.draw_geometries([self.ply, ply_aligned])
+
+        self.ply = ply_aligned
+        return ply_aligned
+
+    def elevation_map(self, overwrite=False, preview=False):
+        ply_np = np.asarray(self.ply.points)
+
+        x_range = ply_np[:, 0].max() - ply_np[:, 0].min()
+        y_range = ply_np[:, 1].max() - ply_np[:, 1].min()
+
+        z_min = ply_np[:, 2].min()
+        z_max = ply_np[:, 2].max()
+        z_range = z_max - z_min
+
+        if not min(x_range, y_range, z_range) == z_range:
+            raise Exception("not aligned with z axis")
+
+        new_colors = []
+        for point in ply_np:
+            new_colors.append([0, (point[2] - z_min) / z_range, 0])
+            # new_colors.append([0.5, 0, 0])
+
+        ply.colors = o3d.utility.Vector3dVector(
+            np.array(new_colors)
+        )
+        ply_new = o3d.geometry.PointCloud()
+
+        ply_new.points = o3d.utility.Vector3dVector(ply_np)
+        ply_new.colors = o3d.utility.Vector3dVector(np.array(new_colors))
+
+        if preview:
+            o3d.visualization.draw_geometries([ply_new])
+        if overwrite:
+            self.ply = ply_new
+
+        return ply_new
+
+    def save(self, name):
+        output_path = Path(os.path.dirname(os.path.abspath(self.ply_path))) / name
+        print("writing to:", output_path)
+        o3d.io.write_point_cloud(str(output_path), self.ply)
+
+
+if __name__ == "__main__":
+    ply_path = "D:/sfm_dense/street_4_3_3/dense/0/fused.ply"
+    ply_path = "D:/sfm_dense/street_4_3_3/dense/0/fused_uniformed_aligned.ply"
+    ply_path = "D:/sfm_dense/street_4_3_3/color_pd30_8bit_small_street_4_3_3.ply"
+
+    ply = CustomPLY(ply_path)
+    # ply.down_sampling(overwrite=True, preview=False)
+    ply.show_range_coords()
+    ply.move_center()
+    ply.show_range_coords()
+    ply.align_to_z(z_=1, preview=False)  # city = 1, sfm = -1
+    # ply.show_range_coords()
+    # ply.save("fused_uniformed_aligned.ply")
+    ply.elevation_map(preview=True)
--- experiments/image_registration/ply_reg_city.py
+++ experiments/image_registration/ply_reg_city.py
@@ -37,3 +37,144 @@ target_down = target.voxel_down_sample(voxel_size=5)
 
 
 o3d.visualization.draw_geometries([target])
+
+
+
+
+def get_plane(ply):
+    plane_model, inliers = ply.segment_plane(
+        distance_threshold=5,
+        ransac_n=1000,
+        num_iterations=1000
+    )
+    [a, b, c, d] = plane_model
+    print(f"Plane equation: {a:.2f}x + {b:.2f}y + {c:.2f}z + {d:.2f} = 0")
+
+    inlier_cloud = ply.select_by_index(inliers)
+    inlier_cloud.paint_uniform_color([1.0, 1.0, 0])
+
+    outlier_cloud = ply.select_by_index(inliers, invert=True)
+    outlier_cloud.paint_uniform_color([1.0, 0, 0])
+
+    o3d.visualization.draw_geometries([inlier_cloud])
+    o3d.visualization.draw_geometries([outlier_cloud])
+
+get_plane(source_scaled_uniformed)
+
+def align_ply_to_z(ply):
+    plane_model, inliers = ply.segment_plane(
+        distance_threshold=5,
+        ransac_n=1000,
+        num_iterations=1000
+    )
+    [a, b, c, d] = plane_model
+    plane_n = np.array([a, b, c])
+    plane_z = np.array([0, 0, -1])
+
+    vec1 = np.reshape(plane_n, (1, -1))
+    vec2 = np.reshape(plane_z, (1, -1))
+
+    from scipy.spatial.transform import Rotation as R
+    r = R.align_vectors(vec2, vec1)
+    rot = r[0].as_matrix()
+
+    ply_aligned = copy.deepcopy(ply)
+    ply_aligned.rotate(rot, center=ply_aligned.get_center())
+    o3d.visualization.draw_geometries([ply, ply_aligned])
+    return ply_aligned
+
+source_scaled_uniformed_aligned = align_ply_to_z(source_scaled_uniformed)
+
+
+
+# street_4_0
+import copy
+import open3d as o3d
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+source = "D:/sfm_dense/street_4_0_1/dense/0/fused.ply"
+target = "D:/sfm_dense/color_pd30_8bit_street_4_0.ply"
+
+source = o3d.io.read_point_cloud(source)
+target = o3d.io.read_point_cloud(target)
+
+source.translate([0, 0, 0], relative=False)
+target.translate([0, 0, 0], relative=False)
+
+def ply_show_range_coords(ply):
+    ply_np = np.asarray(ply.points)
+    print()
+    for i in range(3):
+        print(
+            ply_np[:, i].min(),
+            ply_np[:, i].max(),
+            "range:",
+            ply_np[:, i].max() - ply_np[:, i].min()
+        )
+
+ply_show_range_coords(source)
+ply_show_range_coords(target)
+
+# target
+target_sliced = target.select_by_index(np.where(np.asarray(target.points)[:, 2] < 0)[0])
+o3d.visualization.draw_geometries([target_sliced])
+
+target_sliced_uniformed = target_sliced.voxel_down_sample(voxel_size=1)
+o3d.visualization.draw_geometries([target_sliced_uniformed])
+
+
+# source
+o3d.visualization.draw_geometries([source])
+
+source_scaled = source.scale(20, center=source.get_center())
+
+source_scaled_uniformed = source_scaled.voxel_down_sample(voxel_size=1)
+o3d.visualization.draw_geometries([source_scaled_uniformed])
+
+
+# scaling
+def draw_registration_result(source, target):
+    source_temp = copy.deepcopy(source)
+    target_temp = copy.deepcopy(target)
+    source_temp.paint_uniform_color([1, 0.706, 0])
+    target_temp.paint_uniform_color([0, 0.651, 0.929])
+    o3d.visualization.draw_geometries([source_temp, target_temp])
+
+
+ply_show_range_coords(source_scaled_uniformed)
+ply_show_range_coords(target_sliced_uniformed)
+draw_registration_result(source_scaled_uniformed_aligned, target_sliced_uniformed)
+
+
+
+# save
+
+o3d.io.write_point_cloud("D:/sfm_dense/street_4_0_1/dense/0/fused_scaled_uniformed.ply", source_scaled_uniformed)
+o3d.io.write_point_cloud("D:/sfm_dense/color_pd30_8bit_street_4_0_sliced_uniformed.ply", target_sliced_uniformed)
+
+
+# Test
+# This function is only used to make the keypoints look better on the rendering
+def keypoints_to_spheres(keypoints):
+    spheres = o3d.geometry.TriangleMesh()
+    for keypoint in keypoints.points:
+        sphere = o3d.geometry.TriangleMesh.create_sphere(radius=0.001)
+        sphere.translate(keypoint)
+        spheres += sphere
+    spheres.paint_uniform_color([1.0, 0.75, 0.0])
+    return spheres
+
+source = "D:/sfm_dense/street_4_0_1/dense/0/fused.ply"
+target = "D:/sfm_dense/color_pd30_8bit_street_4_0.ply"
+
+source = o3d.io.read_point_cloud(source)
+keypoints = o3d.geometry.keypoint.compute_iss_keypoints(source,
+                                                        salient_radius=0.5,
+                                                        non_max_radius=1.0,
+                                                        gamma_21=0.5,
+                                                        gamma_32=0.5)
+
+source.paint_uniform_color([0.5, 0.5, 0.5])
+o3d.visualization.draw_geometries([keypoints_to_spheres(keypoints), source])
